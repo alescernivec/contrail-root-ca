8b341a2f04f6795997584bafbc2ab83eacd18deb
root <root@ubuntu.defaultdomain>
2013-11-18 13:00:03 +0000

    updated patch file

--- patch-files.sh
+++ patch-files.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
-CWD=`pwd`
 cd patches
-cp tomcat-server-mods.diff /etc/tomcat6/ && cd /etc/tomcat6/ && patch -p0 < tomcat-server-mods.diff && cd ${CWD}
-cp hosts-mods.diff /etc/ && cd /etc/ && patch -p0 < hosts-mods.diff && cd ${CWD}
-cp ca-server-mods.diff /var/lib/tomcat6/webapps/ca/WEB-INF/ && cd /var/lib/tomcat6/webapps/ca/WEB-INF/ && patch -p0 < ca-server-mods.diff && cd ${CWD}
+CWD=`pwd`
+cp tomcat-server-mods.diff /etc/tomcat6/ && cd /etc/tomcat6/ && patch -p0 < tomcat-server-mods.diff 
+cd ${CWD}
+cp hosts-mods.diff /etc/ && cd /etc/ && patch -p0 < hosts-mods.diff
+cd ${CWD}
+cp ca-server-mods.diff /var/lib/tomcat6/webapps/ca/WEB-INF/ && cd /var/lib/tomcat6/webapps/ca/WEB-INF/ && patch -p0 < ca-server-mods.diff
+cd ${CWD}
+cd ..
