6c243c0d2ce09cd82c4348bc479b2e7198503e24
root <root@ubuntu.defaultdomain>
2013-11-20 11:40:19 +0000

    Missing "cd" in patch-files.sh

--- bin/patch-files.sh
+++ bin/patch-files.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 CWD=`pwd`
 CWD2="$CWD/../patches"
+cd ${CWD2}
 cp tomcat-server-mods.diff /etc/tomcat6/ && cd /etc/tomcat6/ && patch -p0 < tomcat-server-mods.diff 
 cd ${CWD2}
 cp hosts-mods.diff /etc/ && cd /etc/ && patch -p0 < hosts-mods.diff
