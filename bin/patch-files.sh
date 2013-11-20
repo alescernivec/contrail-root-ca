8cfb789e5f9e4f9de80644a1684b0ce50352aff8
root <root@ubuntu.defaultdomain>
2013-11-20 15:27:21 +0000

    updated with the latest patches and additional services

--- bin/patch-files.sh
+++ bin/patch-files.sh
@@ -8,4 +8,14 @@ cp hosts-mods.diff /etc/ && cd /etc/ && patch -p0 < hosts-mods.diff
 cd ${CWD2}
 cp ca-server-mods.diff /var/lib/tomcat6/webapps/ca/WEB-INF/ && cd /var/lib/tomcat6/webapps/ca/WEB-INF/ && patch -p0 < ca-server-mods.diff
 cd ${CWD2}
+cp contrail-federation-web-ssl-mods.diff /etc/apache2/sites-available/ && cd /etc/apache2/sites-available/ patch -p0 < contrail-federation-web-ssl-mods.diff
+cd ${CWD2}
+cp wildcard-simplesaml-ssl-mods.diff /etc/apache2/sites-available/ && cd /etc/apache2/sites-available/ && patch -p0 < wildcard-simplesaml-ssl-mods.diff
+cd ${CWD2}
+cp federation-web-conf-mods.diff /etc/contrail/contrail-federation-web/ && cd /etc/contrail/contrail-federation-web/ && patch -p0 < federation-web-conf-mods.diff
+cd ${CWD2}
+cp saml20-sp-remote.php-mods.diff /usr/share/simplesamlphp-1.9.0/metadata/ && cd /usr/share/simplesamlphp-1.9.0/metadata/ && patch -p0 < saml20-sp-remote.php-mods.diff
+cd ${CWD2}
+cp saml20-idp-hosted.php-mods.diff /usr/share/simplesamlphp-1.9.0/metadata/ && cd /usr/share/simplesamlphp-1.9.0/metadata/ && patch -p0 < saml20-idp-hosted.php-mods.diff
+cd ${CWD2}
 cd $CWD 
--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 ROOTCA_SN="/DC=Slovenia/DC=XLAB/DC=Contrail/DC=ca"
 DEMO_NAME="oauth-client-cred-flow-demo"
 DEMO_LINK="http://bamboo.ow2.org/artifact/CONTRAIL-TRUNK/JOB1/build-latestSuccessful/Packages/common/oauth/oauth-java-client-demo/client-cred-flow/target/contrail-${DEMO_NAME}.tar.gz"
+SERVICES="contrail-security-commons contrail-ca-server contrail-oauth-as contrail-federation-api contrail-federation-web contrail-federation-id-prov-support"
 
 export DEBIAN_FRONTEND="noninteractive"
 
@@ -17,6 +18,23 @@ print_help() {
  
 }
 
+update_certs_idp(){
+	cp -fs /etc/tomcat6/contrail-federation-id-prov-support/contrail-federation-id-prov-support.crt /usr/share/simplesamlphp-1.9.0/cert/wildcard-contrail-idp.cert
+	cp -fs /etc/tomcat6/contrail-federation-id-prov-support/contrail-federation-id-prov-support.key /usr/share/simplesamlphp-1.9.0/cert/wildcard-contrail-idp.key
+	cp -fs /etc/tomcat6/contrail-federation-id-prov-support/contrail-federation-id-prov-support.crt /usr/share/simplesamlphp-1.9.0/cert/server.crt
+	cp -fs /etc/tomcat6/contrail-federation-id-prov-support/contrail-federation-id-prov-support.key /usr/share/simplesamlphp-1.9.0/cert/server.pem
+}
+
+update_certs_web(){
+	cp -fs /etc/tomcat6/contrail-federation-web/contrail-federation-web.crt /usr/lib/contrail/federation-web/extra/contrail-federation-web.cert
+	cp -fs /etc/tomcat6/contrail-federation-web/contrail-federation-web.key /usr/lib/contrail/federation-web/extra/contrail-federation-web.key
+	cp -fs /var/lib/contrail/ca-server/rootca-cert.pem /usr/lib/contrail/federation-web/extra/ca.crt
+}
+
+update_web_conf(){
+	METADATA=`curl --insecure https://contrail-federation-id-prov-support/simplesaml/saml2/idp/metadata.php` 
+	echo -e "<?xml version="1.0"?>\n<md:EntitiesDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata" xmlns:ds="http://www.w3.org/2000/09/xmldsig#">${METADATA}\n</md:EntitiesDescriptor>" > /usr/lib/contrail/federation-web/extra/remote_metadata.xml
+}
 
 if [ $# -ne $EXPECTED_ARGS ]
 then
@@ -28,15 +46,26 @@ CONFIG=$1
 
 if [ ${CONFIG} == "notest" ]
 then
-	echo "Installing ConSec on localhost"
-	apt-get install contrail-ca-server contrail-federation-api contrail-oauth-as contrail-security-commons
+	echo "Installing ConSeC on localhost"
+	apt-get install ${SERVICES}
 	create-rootca-files ${ROOTCA_SN}
 	cd bin
 	./create_ca.sh
 	./create_service_certs.sh
 	./patch-files.sh
 	cd ..
+	echo "Updating certificates for contrail-idp"
+	update_certs_idp
+	echo "Updating certificates for contrail-federation-web"
+	update_certs_web
 	service tomcat6 restart
+        service apache2 reload
+	echo "Sleeping for 20s (tomcat needs to bootstrap)..."
+	sleep 20
+	echo "...done"
+	update_web_conf
+	service tomcat6 restart
+	service apache2 reload
 	exit 0
 elif [ ${CONFIG} == "test" ]
 then
