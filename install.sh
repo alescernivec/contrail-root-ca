8e06ebdb63a4e0618dde19f1d4d8a7db0f70508e
root <root@ubuntu.defaultdomain>
2013-11-20 12:18:56 +0000

    Updates to install script

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ROOTCA_SN="/DC=Slovenia/DC=XLAB/DC=Contrail/DC=ca"
-DEMO_NAME="contrail-oauth-client-cred-flow-demo"
-DEMO_LINK="http://bamboo.ow2.org/artifact/CONTRAIL-TRUNK/JOB1/build-latestSuccessful/Packages/common/oauth/oauth-java-client-demo/client-cred-flow/target/${DEMO_NAME}.tar.gz"
+DEMO_NAME="oauth-client-cred-flow-demo"
+DEMO_LINK="http://bamboo.ow2.org/artifact/CONTRAIL-TRUNK/JOB1/build-latestSuccessful/Packages/common/oauth/oauth-java-client-demo/client-cred-flow/target/contrail-${DEMO_NAME}.tar.gz"
 
 EXPECTED_ARGS=1
 E_BADARGS=65
@@ -39,10 +39,10 @@ then
 elif [ ${CONFIG} == "test" ]
 then
 	wget ${DEMO_LINK}
-	tar xvfz ${DEMO_NAME}.tar.gz
+	tar xvfz contrail-${DEMO_NAME}.tar.gz
 	mkdir -p /usr/share/contrail/${DEMO_NAME}/lib
 	mkdir -p /etc/contrail/${DEMO_NAME}/
-	cd ${DEMO_NAME}
+	cd contrail-${DEMO_NAME}
 	cp -r etc/* /etc/
 	cp -r usr/* /usr/
 	cd ..
