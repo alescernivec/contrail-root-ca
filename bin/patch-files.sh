9f3bc2bc81f0d38d0cf9eaff87be898ceab2fb68
root <root@ubuntu.defaultdomain>
2013-11-20 10:05:25 +0000

    Modified patch-files due to pwd errs.

--- bin/patch-files.sh
+++ bin/patch-files.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
-
-cd patches
 CWD=`pwd`
+CWD2="$CWD/../patches"
 cp tomcat-server-mods.diff /etc/tomcat6/ && cd /etc/tomcat6/ && patch -p0 < tomcat-server-mods.diff 
-cd ${CWD}
+cd ${CWD2}
 cp hosts-mods.diff /etc/ && cd /etc/ && patch -p0 < hosts-mods.diff
-cd ${CWD}
+cd ${CWD2}
 cp ca-server-mods.diff /var/lib/tomcat6/webapps/ca/WEB-INF/ && cd /var/lib/tomcat6/webapps/ca/WEB-INF/ && patch -p0 < ca-server-mods.diff
-cd ${CWD}
-cd ..
+cd ${CWD2}
+cd $CWD 
